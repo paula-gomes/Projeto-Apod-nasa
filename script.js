7f21dddf73ee4e43e96f67f566674007fd76984e
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-04 19:07:08 -0300

    adicionando evento form

--- script.js
+++ script.js
@@ -1,34 +1,42 @@
 
-let reqData= new XMLHttpRequest();
-reqData.open("GET", "https://api.nasa.gov/planetary/apod?api_key=8MPA5HdNgriAe4ogycoL2jsGcBfSFtfm01igpGwk");
-//tratar os dados antes de enviar a requisição//
-reqData.onload= function (){
 
-   if(reqData.status==200)
-    { let reqResult=JSON.parse(reqData.responseText);    
+let suBmit= document.querySelector("#sub");
+let dateForm= document.querySelector("#formDate");
 
-    let dateOfMedia= document.querySelector("#date");
-    dateOfMedia.textContent+=reqResult.date;
-
-    let picOfDay= document.querySelector("#mediaOftheDay");
-    picOfDay.src=reqResult.url;  
-
-    let explainPic=document.querySelector("#explanation");
-    explainPic.textContent+=reqResult.explanation;
-           
-    let titleOfPic=document.querySelector("#titleOfimg");
-    titleOfPic.textContent=reqResult.title;
-
-    let cpyRightOfPic= document.querySelector("#cpyRight");
-    cpyRightOfPic.textContent+= reqResult.copyright;}
-    else
-    { alert("Não foi possível completar a sua requisição")}
+dateForm.addEventListener("submit",function (event){
     
-}
+    event.preventDefault();
+    console.log("oi");
+    let reqData= new XMLHttpRequest();
+    reqData.open("GET", `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.elements.dia.value}`);
+    //tratar os dados antes de enviar a requisição//
+    reqData.onload= function (){
 
-reqData.send();
+    if(reqData.status==200)
+        { let reqResult=JSON.parse(reqData.responseText);    
 
+        let dateOfMedia= document.querySelector("#date");
+        dateOfMedia.textContent+=reqResult.date;
 
+        let picOfDay= document.querySelector("#mediaOftheDay");
+        picOfDay.src=reqResult.url;  
+
+        let explainPic=document.querySelector("#explanation");
+        explainPic.textContent+=reqResult.explanation;
+            
+        let titleOfPic=document.querySelector("#titleOfimg");
+        titleOfPic.textContent=reqResult.title;
+
+        let cpyRightOfPic= document.querySelector("#cpyRight");
+        cpyRightOfPic.textContent+= reqResult.copyright;}
+        else
+        { alert("Não foi possível completar a sua requisição")}
+        
+    }
+
+    reqData.send();
+
+});
 
 
 
