a1b485bd1d1f30feed149ecb376c67c1bca4f69a
Paula Gomes <gomes.pcristina@gmail.com>
2020-07-06 23:57:01 -0300

    adição de classes para mediatype

--- script.js
+++ script.js
@@ -1,34 +1,56 @@
 
 
-let suBmit= document.querySelector("#sub");
+let suBmit= document.querySelector("#submit");
 let dateForm= document.querySelector("#formDate");
 
 dateForm.addEventListener("submit",function (event){
     
     event.preventDefault();
-    console.log("oi");
+
     let reqData= new XMLHttpRequest();
-    reqData.open("GET", `https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=${this.elements.dia.value}`);
+    reqData.open("GET", `https://api.nasa.gov/planetary/apod?api_key=8MPA5HdNgriAe4ogycoL2jsGcBfSFtfm01igpGwk&date=${this.elements.day.value}`);
+    
+    
     //tratar os dados antes de enviar a requisição//
     reqData.onload= function (){
+            
+    let picOfDay= document.querySelector("#imgOftheDay"); 
+    let vidOfDay= document.querySelector("#vidOfTheDay");
 
     if(reqData.status==200)
         { let reqResult=JSON.parse(reqData.responseText);    
 
         let dateOfMedia= document.querySelector("#date");
-        dateOfMedia.textContent+=reqResult.date;
+        dateOfMedia.textContent= `Date: `+reqResult.date;
 
-        let picOfDay= document.querySelector("#mediaOftheDay");
-        picOfDay.src=reqResult.url;  
+        if(reqResult.media_type == "image")
+        {
+            
+            picOfDay.classList.remove("hideImg");            
+            picOfDay.src=reqResult.url;             
+            vidOfDay.classList.add("hideVid");
+           
+        }
+        else
+        {
+            vidOfDay.classList.remove("hideVid");            
+            vidOfDay.src=reqResult.url;
+            picOfDay.classList.add("hideImg");
+            
+        }
+        
+        picOfDay.classList.add("showImg");
+        vidOfDay.classList.add("showVid");
+
 
         let explainPic=document.querySelector("#explanation");
-        explainPic.textContent+=reqResult.explanation;
+        explainPic.textContent=`Explanation: `+ reqResult.explanation;
             
         let titleOfPic=document.querySelector("#titleOfimg");
         titleOfPic.textContent=reqResult.title;
 
         let cpyRightOfPic= document.querySelector("#cpyRight");
-        cpyRightOfPic.textContent+= reqResult.copyright;}
+        cpyRightOfPic.textContent=`Copyright: ` + reqResult.copyright;}
         else
         { alert("Não foi possível completar a sua requisição")}
         
